9f51a1035e4a89ae26cf44be68e5664d254b11a3
ArmandoSJ <just-jauregui@hotmail.com>
2025-07-30 21:54:44 -0600

    add splash screen component

---
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -0,0 +1,8 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'splash-screen',
+  imports: [],
+  templateUrl: './splash-screen.component.html'
+})
+export class SplashScreenComponent {}
