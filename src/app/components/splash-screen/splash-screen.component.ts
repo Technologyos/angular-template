8be348ca620266f6360cf90c654e61bd5950f82f
ArmandoSJ <just-jauregui@hotmail.com>
2025-09-21 18:22:50 -0600

    feat: remove splash screen component

--- src/app/components/splash-screen/splash-screen.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component } from '@angular/core';
-
-@Component({
-  selector: 'splash-screen',
-  imports: [],
-  templateUrl: './splash-screen.component.html'
-})
-export class SplashScreenComponent {}
